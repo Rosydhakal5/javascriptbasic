329b884837855989e3b7f4ae0178285de292a62c
Rosy <rosydhakal333@gmail.com>
2023-12-05 19:51:56 +1100

    OOP more learning

--- class.js
+++ class.js
@@ -10,11 +10,26 @@ class Person{
         this.age = myAge;
     }
     readBio(){
-        console.log("this is my name", this.name)
+        console.log("this is my name", this.name, "this is my age", this.age)
     }
 }
 
 const person1 = new Person("Rosy", 23);
 person1.readBio();
 
+const person2 = new Person("Patsey", 28);
+person2.readBio();
 
+const person3 = new Person("apple", 3000)
+person3.readBio();
+
+//<--- if we write this. something in child it takes whatever is there in its parents value---->
+
+const person5 = {
+    name: "Rosy Dhakal",
+    age: 23,
+    readBio: function(){
+        console.log("this is my name", this.name, "and my age is", this.age);
+    }
+}
+person5.readBio();
