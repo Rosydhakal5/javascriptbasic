8a0e3e0c43329bfcc1b3a3e9752f471bcd71bd01
Rosy <rosydhakal333@gmail.com>
2023-12-05 20:31:40 +1100

    Inheritance OOP

--- class.js
+++ class.js
@@ -28,6 +28,7 @@ person3.readBio();
 const person5 = {
     name: "Rosy Dhakal",
     age: 23,
+    address: "KTM",
     readBio: function(){
         console.log("this is my name", this.name, "and my age is", this.age);
     },
@@ -36,7 +37,31 @@ const person5 = {
     // so its better to refer directly the object name and access the details 
     readName: () => {
         console.log("this is my name", person5.name)
+    },
+    completeBio: () =>{
+        return "My name is"+ person5.name + "i live in "+ person5.address
     }
+
 }
 person5.readBio();
 person5.readName();
+console.log(person5.completeBio())
+
+
+//Inheritance 
+//extend is a reserve keyword that help us to read details from another
+//
+class Student extends Person{
+    //gets called when creating an object
+    constructor(fullName, age){
+        super(); //if we don't provide this it will throw an error due to adding 
+        //constructor in the class,  super here is parent which is person, so we are 
+        //bascially forwarding it to parent
+        this.type = "student"
+    }
+}
+    const student1  = new Student("Rihana", 20)
+    student1.readBio();
+class Employee extends Person{}
+const Employee1 = new Employee("Peter", 3)
+Employee1.readBio();
